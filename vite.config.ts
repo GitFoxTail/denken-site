b6f409a4f929a814e760a0217e7b222140f8ac36
Yuji Matsumoito <heiseirider20@yahoo.co.jp>
2026-04-20 21:50:28 +0900

    ビルドの分割

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,21 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules/react-katex') || id.includes('node_modules/katex')) {
+            return 'katex'
+          }
+          if (id.includes('node_modules/react/') || id.includes('node_modules/react-dom/')) {
+            return 'react-vendor'
+          }
+          if (id.includes('node_modules/react-router-dom/')) {
+            return 'router'
+          }
+        },
+      },
+    },
+  },
 })
